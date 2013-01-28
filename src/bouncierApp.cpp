32e7c4464075758683860c24f9ddad8f8ca34de3
Marione John R. Semilla <marione-jrs312@hotmail.com>
2013-01-28 20:54:48 +0800

    auto into appropos

--- src/bouncierApp.cpp
+++ src/bouncierApp.cpp
@@ -4,6 +4,7 @@
 #include <vector>
 #include <algorithm>
 #include <iostream>
+#include <bitset>
 
 using namespace ci;
 using namespace ci::app;
@@ -93,6 +94,87 @@ struct Circle {
 	float mass() const { return radius * radius * 3.14159265358979323846264338327 + 1; }
 };
 
+struct Bit {
+	unsigned long int b : 1;
+};
+
+/*	
+class BitGrid
+{
+	const float world_width, world_height;
+	const size_t cols, rows;
+	const float cell_width, cell_height;
+	
+	Bit *rowbits;
+	Bit *colbits;
+	
+	vector<Circle> circles;
+	
+	public:
+	
+	BitGrid(float world_width, float world_height, size_t cols, size_t rows) :
+	world_width(world_width), world_height(world_height), cols(cols), rows(rows),
+	cell_width(world_width/cols), cell_height(world_height/rows) {
+		rowbits= new Bit[rows];
+		colbits= new Bit[cols]; 
+	}
+		
+	const size_t nRows() const { return rows; }
+	const size_t nCols() const { return cols; }
+	const size_t cellWidth() const { return cell_width; }
+	const size_t cellHeight() const { return cell_height; }
+	
+	size_t findRowPosition ( float y )
+	{
+		return clamp((int)floor(y / cell_height), 0, (int)(rows - 1));
+	}
+	
+	size_t findColumnPosition ( float x )
+	{
+		return clamp((int)floor(x / cell_width), 0, (int)(cols - 1));
+	}
+	
+
+	
+	//puts objects into its respective bitsets
+	void put ( Circle &c )
+	{
+		//find row and column position of the circle
+		const size_t row_loc = findRowPosition(c.center.y), 
+		col_loc = findColumnPosition(c.center.x);
+		
+		rowbits[row_loc].b = 1;
+		colbits[col_loc].b = 1;
+		
+		//push back to circle
+		circles.push_back(c);
+	}
+	
+	vector<Circle*> getObjectsInRect(float centerX, float centerY, float w, float h) {
+		vector<Circle*> ret;
+		w *= 0.5;
+		h *= 0.5;
+		const int
+		x_min = clamp(int((centerX - w)/cell_width), 0, (int)cols-1),
+		x_max = clamp(int((centerX + w)/cell_width), 0, (int)cols-1),
+		
+		y_min = clamp(int((centerY - h)/cell_height), 0, (int)rows-1),
+		y_max = clamp(int((centerY + h)/cell_height), 0, (int)rows-1);
+		
+		for ( int c = x_min; c <= x_max; ++c ) {
+			for ( int r = y_min; r <= y_max; ++r ) {
+				
+				ret.push_back();
+			}
+		}
+		return ret;
+	}
+	
+	
+	
+};
+*/
+
 class UniformGrid {
 	const float world_width, world_height;
 	const size_t cols, rows;
@@ -104,7 +186,6 @@ public:
 	UniformGrid(float world_width, float world_height, size_t cols, size_t rows) :
 	world_width(world_width), world_height(world_height), cols(cols), rows(rows),
 	cell_width(world_width/cols), cell_height(world_height/rows), grid(rows * cols) {
-		
 	}
 	
 	const size_t nRows() const { return rows; }
@@ -119,7 +200,7 @@ public:
 	}
 	
 	size_t getCellIndex(size_t c, size_t r ) const {
-		auto index = cols * r + c;
+		size_t index = cols * r + c;
 		assert( index < cols * rows );
 		return index;
 	}
@@ -129,7 +210,7 @@ public:
 	}
 	
 	vector<Circle*>& getCellForPosition(float x, float y) {
-		auto idx = getCellIndexForPosition(x, y);
+		size_t idx = getCellIndexForPosition(x, y);
 		return grid[idx];
 	}
 	
@@ -162,7 +243,7 @@ public:
 		
 		for ( int c = x_min; c <= x_max; ++c ) {
 			for ( int r = y_min; r <= y_max; ++r ) {
-				const auto &cell = getCell(c, r);
+				const vector<Circle*> &cell = getCell(c, r);
 				ret.insert(ret.end(), cell.begin(), cell.end());
 			}
 		}
@@ -187,7 +268,7 @@ class bouncierApp : public AppBasic {
 
 void bouncierApp::setup()
 {
-	auto screenWidth = this->getWindowWidth(), screenHeight = this->getWindowHeight();
+	float screenWidth = this->getWindowWidth(), screenHeight = this->getWindowHeight();
 	ug = new UniformGrid(screenWidth, screenHeight, 10, 10);
 	for ( size_t i = 0; i < N_BALLS; ++i ) {
 		circles[i].center.y = screenHeight * randFloat();
@@ -209,7 +290,7 @@ void bouncierApp::update()
 	const float screenWidth = this->getWindowWidth(), screenHeight = this->getWindowHeight();
 	for ( size_t i = 0; i < N_BALLS; ++i ) {
 		Circle &c = circles[i];
-		const auto prevCenter = c.center;
+		const Vector2 prevCenter = c.center;
 		c.center += c.vel;
 		
 		if ( c.center.y - c.radius <= 0 ) {
@@ -228,7 +309,7 @@ void bouncierApp::update()
 			c.center.x = screenWidth - c.radius;
 		}
 		
-		auto candidates = ug->getObjectsInRect(prevCenter.x, prevCenter.y, ug->cellWidth() * 2, ug->cellHeight() * 2);
+		vector<Circle*> candidates = ug->getObjectsInRect(prevCenter.x, prevCenter.y, ug->cellWidth() * 2, ug->cellHeight() * 2);
 		
 		for ( size_t j = 0; j < candidates.size(); ++j ) {
 			Circle &o = *candidates[j];
@@ -236,17 +317,17 @@ void bouncierApp::update()
 				continue;
 			}
 
-			auto d = o.center - c.center;
+			Vector2 d = o.center - c.center;
 			float dsq = d.sqMag();
 			if ( dsq <= (c.radius + o.radius) * (c.radius + o.radius) ) {
-				auto mag = sqrt(dsq);
+				float mag = sqrt(dsq);
 				d.x = d.x / mag;
 				d.y = d.y / mag;
 				
 				Vector2 oprime = (o.vel * d) * d,
 				cprime = (c.vel * d) * d;
 				
-				auto invTotalMass = 1.0f/(o.mass() + c.mass());
+				float invTotalMass = 1.0f/(o.mass() + c.mass());
 				Vector2 newO = (o.vel - oprime) +
 				(o.mass() - c.mass()) * invTotalMass * oprime +
 				2 * c.mass() * invTotalMass * cprime,
@@ -256,20 +337,20 @@ void bouncierApp::update()
 				o.vel = newO;
 				c.vel = newC;
 				
-				auto w = (c.radius + o.radius - mag) * 0.5;
+				float w = (c.radius + o.radius - mag) * 0.5;
 				
-				const auto prevOCenter = o.center;
+				const Vector2 prevOCenter = o.center;
 				o.center += d * w;
 				c.center -= d * w;
 				
 				//update the bin of o
-				auto &OoldCell = ug->getCellForPosition(prevOCenter.x, prevOCenter.y);
+				vector<Circle*> &OoldCell = ug->getCellForPosition(prevOCenter.x, prevOCenter.y);
 				OoldCell.erase(find(OoldCell.begin(), OoldCell.end(), &o));
 				ug->put(o);
 			}
 		}
 		//update the bin of c
-		auto &oldCell = ug->getCellForPosition(prevCenter.x, prevCenter.y);
+		vector<Circle*> &oldCell = ug->getCellForPosition(prevCenter.x, prevCenter.y);
 		oldCell.erase(find(oldCell.begin(), oldCell.end(), &c));
 		ug->put(c);
 
